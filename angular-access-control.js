d9705227c3bdc9c9e9e6769ce0e3de1c08248cde
Ates Goral <ates.g@myplanet.com>
2015-06-04 01:41:50 -0400

    Changed API, caching annotations, added UMD support

--- angular-access-control.js
+++ angular-access-control.js
@@ -3,6 +3,8 @@
         define([ 'module', 'angular' ], function (module, angular) {
             module.exports = factory(angular);
         });
+    } else if (typeof module === 'object') {
+        module.exports = factory(require('angular'));
     } else {
         if (!root.mp) {
             root.mp = {};
@@ -13,44 +15,61 @@
 }(this, function (angular) {
     'use strict';
 
-    function AccessControlPolicies(policies) {
-        this.get = function () {
-            return policies;
-        };
-    }
-
-    function AccessControlPoliciesProvider() {
-        var policies = [];
-
-        this.add = function (policy) {
-            policies.push(policy);
-            return this;
-        };
-
-        this.$get = function accessControlPoliciesFactory() {
-            return new AccessControlPolicies(policies);
-        };
-    }
-
     return angular.module('mp.accessControl', [])
-        .provider('$accessControlPolicies', AccessControlPoliciesProvider)
-        .directive('acIf', [ '$accessControlPolicies', '$injector', function ($accessControlPolicies, $injector) {
-            var policies = null;
+        .provider('$accessControlPolicies', function () {
+            var policyFactories = [],
+                policies;
 
+            this.policy = function (policyFactory) {
+                policyFactories.push(policyFactory);
+                return this;
+            };
+
+            this.$get = [ '$injector', function accessControlPoliciesFactory($injector) {
+                return {
+                    allow: function (scope, element, attrs) {
+                        if (!policies) {
+                            policies = policyFactories.map(function (policyFactory) {
+                                return $injector.invoke(policyFactory);
+                            });
+                        }
+
+                        return policies.every(function (policy) {
+                            return policy.allows(scope, element, attrs);
+                        });
+                    }
+                };
+            }];
+        })
+        .directive('acIf', [ '$accessControlPolicies', '$compile', function ($accessControlPolicies, $compile) {
+            return {
+                priority: 700,
+                restrict: 'A',
+                terminal: true,
+                scope: true,
+
+                compile: function (element) {
+                    var ifExpr = element.attr('ng-if');
+
+                    element.removeAttr('ac-if');
+                    element.attr('ng-if', '$$isAccessible' + (ifExpr ? ' && (' + ifExpr + ')' : ''));
+
+                    return {
+                        pre: function (scope, element, attrs) {
+                            scope.$$isAccessible = $accessControlPolicies.allow(scope, element, attrs);
+                            $compile(element)(scope);
+                        }
+                    };
+                }
+            };
+        }])
+        .directive('acUnwrap', [ '$accessControlPolicies', function ($accessControlPolicies) {
             return {
                 priority: 500,
                 restrict: 'A',
                 link: function (scope, element, attrs) {
-                    if (!policies) {
-                        policies = $accessControlPolicies.get().map(function (policyFactory) {
-                            return $injector.invoke(policyFactory);
-                        });
-                    }
-
-                    if (!policies.every(function (policy) {
-                        return policy.apply(scope, element, attrs);
-                    })) {
-                        element.remove();
+                    if (!$accessControlPolicies.allow(scope, element, attrs)) {
+                        element.replaceWith(element.contents());
                     }
                 }
             };
